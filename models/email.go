dd0ed13a477bafc586d58ba24bd2ae8d3efa97fb
Raajheer1 <raaj.patel229@gmail.com>
2022-12-23 17:19:25 -0600

    I made an oopsies

--- models/email.go
+++ models/email.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Email struct {
-	ID          uint `json:"id" gorm:"primaryKey;autoIncrement" example:"1"`
-	CompanyID   uint `json:"company"`
-	Company     Company
-	EmailType   uint `json:"email_type"`
-	EmailTypeID EmailTemplate
-	CreatedAt   time.Time `json:"created_at" example:"2020-01-01T00:00:00Z"`
-	UpdatedAt   time.Time `json:"updated_at" example:"2020-01-01T00:00:00Z"`
+	ID              uint `json:"id" gorm:"primaryKey;autoIncrement" example:"1"`
+	CompanyID       uint `json:"company"`
+	Company         Company
+	EmailTemplateID uint `json:"email_template"`
+	EmailTemplate   EmailTemplate
+	CreatedAt       time.Time `json:"created_at" example:"2020-01-01T00:00:00Z"`
+	UpdatedAt       time.Time `json:"updated_at" example:"2020-01-01T00:00:00Z"`
 }
