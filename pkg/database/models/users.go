8d87daed1c2a44bc7d5517c964746dc551582756
jamesn8391 <jamesn8391@gmail.com>
2023-01-03 22:28:08 -0600

    remove created at and updated at tags in json

--- pkg/database/models/users.go
+++ pkg/database/models/users.go
@@ -7,8 +7,6 @@ import (
 type Login struct {
 	UserKey   string    `json:"userkey" example:"keyName"`
 	Email     string    `json:"email" example:"username@email.com"`
-	CreatedAt time.Time `json:"created_at" example:"2021-09-01T00:00:00Z"`
-	UpdatedAt time.Time `json:"updated_at" example:"2021-09-01T00:00:00Z"`
 }
 
 type Register struct {
@@ -16,12 +14,8 @@ type Register struct {
 	LastName  string    `json:"lastname" example:"Smith"`
 	Email     string    `json:"email" example:"username@email.com"`
 	Company   string    `json:"company" example:"CompanyName"`
-	CreatedAt time.Time `json:"created_at" example:"2021-09-01T00:00:00Z"`
-	UpdatedAt time.Time `json:"updated_at" example:"2021-09-01T00:00:00Z"`
 }
 
 type Logout struct {
 	UserKey   string    `json:"userkey" example:"keyName"`
-	CreatedAt time.Time `json:"created_at" example:"2021-09-01T00:00:00Z"`
-	UpdatedAt time.Time `json:"updated_at" example:"2021-09-01T00:00:00Z"`
 }
