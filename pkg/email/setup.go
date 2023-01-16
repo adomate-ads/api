f760816d6a13240a8d90f4c53b0fd0b886790dfe
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2023-01-15 18:13:31 -0600

    RabbitMQ Email System

--- pkg/email/setup.go
+++ pkg/email/setup.go
@@ -2,33 +2,83 @@ package email
 
 import (
 	"context"
-	"github.com/mailgun/mailgun-go/v4"
+	"encoding/json"
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
 	"os"
 	"time"
 )
 
-var domain string = "adomate.com"
-
-var private_key string = os.Getenv("EMAIL_PRIVATE_KEY")
-
-var mg *mailgun.MailgunImpl
-var sender string
-
-func Setup() {
-	mg = mailgun.NewMailgun(domain, private_key)
-	sender = "no-reply@adomate.com"
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
 }
 
-func SendEmail(to string, subject string, body string) (string, string) {
-	message := mg.NewMessage(sender, subject, body, to)
+type RabbitMQConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Queue    string
+}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
+type Email struct {
+	To      string
+	Subject string
+	Body    string
+}
 
-	resp, id, err := mg.Send(ctx, message)
-	if err != nil {
-		return "", ""
+func SendEmail(to string, subject string, body string) {
+	RMQConfig := &RabbitMQConfig{
+		Host:     os.Getenv("RABBITMQ_HOST"),
+		Port:     os.Getenv("RABBITMQ_PORT"),
+		User:     os.Getenv("RABBITMQ_USER"),
+		Password: os.Getenv("RABBITMQ_PASSWORD"),
+		Queue:    os.Getenv("RABBIT_MAIL_QUEUE"),
 	}
 
-	return resp, id
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", RMQConfig.User, RMQConfig.Password, RMQConfig.Host, RMQConfig.Port))
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		RMQConfig.Queue, // name
+		true,            // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	email := &Email{
+		To:      to,
+		Subject: subject,
+		Body:    body,
+	}
+
+	message, err := json.Marshal(email)
+	failOnError(err, "Failed to marshal email")
+
+	err = ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			// TODO: Is this the right content type? Maybe application/json or text/html not sure...?
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	failOnError(err, "Failed to publish a message")
 }
