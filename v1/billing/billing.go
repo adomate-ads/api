c7165fa77e1aa6edbab58b30ff131971a3da842e
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2023-01-27 12:51:03 -0600

    Start Stripe implementation

--- v1/billing/billing.go
+++ v1/billing/billing.go
@@ -1,10 +1,13 @@
 package billing
 
 import (
+	"fmt"
 	"github.com/adomate-ads/api/models"
 	"github.com/adomate-ads/api/pkg/auth"
 	"github.com/adomate-ads/api/pkg/email"
 	"github.com/gin-gonic/gin"
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/customer"
 	"net/http"
 	"strconv"
 	"strings"
@@ -70,6 +73,20 @@ func CreateBilling(c *gin.Context) {
 	if err := b.CreateBilling(); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
+	}
+
+	params := &stripe.CustomerSearchParams{}
+	params.Query = *stripe.String(fmt.Sprintf("metadata['company_id']:'%s'", company.ID))
+	// TODO - grab the customer ID from stripe
+	customerID := "12345"
+	iter := customer.Search(params)
+	for iter.Next() {
+		result := iter.Current()
+
+	}
+	billParams := &stripe.InvoiceParams{
+		Customer:    stripe.String(customerID),
+		Description: "10/12-12/10 something like that..",
 	}
 
 	email.SendEmail(company.Email, email.Templates["new-invoice"].Subject, email.Templates["new-invoice"].Body)
