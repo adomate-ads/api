44cdac1ac2ba9208886dd64a4205b9e23de922c8
Raaj Patel <41763998+Raajheer1@users.noreply.github.com>
2023-04-21 01:07:16 -0500

    Tons of stuff (#42)

--- main.go
+++ main.go
@@ -10,10 +10,11 @@ import (
 	google_ads "github.com/adomate-ads/api/pkg/google-ads"
 	"github.com/adomate-ads/api/pkg/openai"
 	"github.com/adomate-ads/api/pkg/stripe"
-	"github.com/adomate-ads/api/pkg/ws"
+	website_parse "github.com/adomate-ads/api/pkg/website-parse"
 	"github.com/adomate-ads/api/v1/billing"
 	"github.com/adomate-ads/api/v1/campaign"
 	"github.com/adomate-ads/api/v1/company"
+	get_started "github.com/adomate-ads/api/v1/get-started"
 	gads "github.com/adomate-ads/api/v1/google-ads"
 	"github.com/adomate-ads/api/v1/industry"
 	"github.com/adomate-ads/api/v1/order"
@@ -29,7 +30,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
 )
 
 // OnlineCheck godoc
@@ -69,6 +69,8 @@ func main() {
 	google_ads.Setup()
 	stripe.Setup()
 	stripe.SetupProducts()
+	website_parse.Setup()
+
 	//stripe.GetSubscriptions()
 
 	discord.Setup()
@@ -80,17 +82,6 @@ func main() {
 	r.Use(gin.Logger())
 
 	r.Use(auth.Auth)
-
-	// Add Websocket Connection
-	const numWorkers = 15
-	var wg sync.WaitGroup
-	jobs := make(chan ws.Job, numWorkers)
-	for i := 0; i < numWorkers; i++ {
-		go ws.Worker(&wg, jobs)
-	}
-	r.GET("/ws", func(c *gin.Context) {
-		ws.Wshandler(c.Writer, c.Request, jobs, &wg)
-	})
 
 	// Add router group for v1
 	v1 := r.Group("/v1")
@@ -117,6 +108,7 @@ func main() {
 	service.Routes(v1)
 	preregistration.Routes(v1)
 	gads.Routes(v1)
+	get_started.Routes(v1)
 
 	// Static files, such as images, css, and js
 	v1.StaticFS("/storage", gin.Dir("./storage", false))
