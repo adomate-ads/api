48a934fd638a8c0a0cf414662a555b993cece13d
Raajheer1 <raaj.patel229@gmail.com>
2023-05-17 21:33:19 -0500

    Add more debug code..

--- pkg/stripe/webhooks/get-started.go
+++ pkg/stripe/webhooks/get-started.go
@@ -2,33 +2,21 @@ package webhooks
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/adomate-ads/api/models"
 	"github.com/adomate-ads/api/pkg/discord"
 	"github.com/adomate-ads/api/pkg/email"
 	"github.com/google/uuid"
 	"github.com/stripe/stripe-go/v74"
-	"github.com/stripe/stripe-go/v74/paymentintent"
 )
 
 func PaymentSucceeded(paymentIntent stripe.PaymentIntent) {
-	discord.SendMessage(discord.Log, "Stripe Webhook - Payment Succeeded", "")
-
-	fmt.Println(paymentIntent.Customer.Email)
-	fmt.Println(paymentIntent.Customer)
-
-	fmt.Println("Break...")
-	params := &stripe.PaymentIntentParams{}
-	params.AddExpand("customer")
-
-	pi, err := paymentintent.Get(paymentIntent.ID, params)
-	if err != nil {
-		discord.SendMessage(discord.Error, "Stripe Webhook Payment Error", "Attempted to get payment intent, but failed: "+err.Error())
-	}
-
-	user, err := models.GetUserByEmail(pi.Customer.Email)
+	user, err := models.GetUserByEmail(paymentIntent.Customer.Email)
 	if err != nil {
 		discord.SendMessage(discord.Error, "Stripe Webhook Payment Error", "Attempted to search for user by email, but no user found: "+paymentIntent.Customer.Email)
+		str, _ := json.MarshalIndent(paymentIntent, "", "  ")
+		fmt.Println(str)
 		return
 	}
 
