1df97db86b0b96213d3febbbf749b94304c033e8
Raaj Patel <41763998+Raajheer1@users.noreply.github.com>
2023-08-17 15:37:09 -0500

    Migrate to Postgres (#52)
    * Migrate to Postgres
    
    * Add error check

--- models/setup.go
+++ models/setup.go
@@ -2,8 +2,8 @@ package models
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 	"os"
 )
@@ -29,10 +29,11 @@ func Config() *DBConfig {
 }
 
 func ConnectDatabase(dbConfig *DBConfig, clearDB bool) {
-	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
+	//DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbConfig.Host, dbConfig.User, dbConfig.Password, dbConfig.Database, dbConfig.Port)
 
 	var err error
-	DB, err = gorm.Open("mysql", DBURL)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Cannot connect to database.")
 		log.Fatal("Connection Error:", err)
@@ -40,29 +41,8 @@ func ConnectDatabase(dbConfig *DBConfig, clearDB bool) {
 		fmt.Println("Connected to database.")
 	}
 
-	if clearDB {
-		DB.DropTableIfExists(&User{})
-		DB.DropTableIfExists(&Company{})
-		DB.DropTableIfExists(&Industry{})
-		DB.DropTableIfExists(&EmailTemplate{})
-		DB.DropTableIfExists(&Email{})
-		DB.DropTableIfExists(&Campaign{})
-		DB.DropTableIfExists(&Order{})
-		DB.DropTableIfExists(&Billing{})
-		DB.DropTableIfExists(&PasswordReset{})
-		DB.DropTableIfExists(&Location{})
-		DB.DropTableIfExists(&Service{})
+	err = DB.AutoMigrate(&User{}, &Company{}, &Industry{}, &EmailTemplate{}, &Email{}, &Campaign{}, &Order{}, &Billing{}, &PasswordReset{}, &Location{}, &Service{})
+	if err != nil {
+		log.Fatal("Cannot auto-migrate db:", err)
 	}
-
-	DB.AutoMigrate(&User{})
-	DB.AutoMigrate(&Company{})
-	DB.AutoMigrate(&Industry{})
-	DB.AutoMigrate(&EmailTemplate{})
-	DB.AutoMigrate(&Email{})
-	DB.AutoMigrate(&Campaign{})
-	DB.AutoMigrate(&Order{})
-	DB.AutoMigrate(&Billing{})
-	DB.AutoMigrate(&PasswordReset{})
-	DB.AutoMigrate(&Location{})
-	DB.AutoMigrate(&Service{})
 }
