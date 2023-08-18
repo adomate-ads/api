5b6e638233696c21a354ad2004b400e0f48b8a8f
jamesn8391 <jamesn8391@gmail.com>
2023-08-18 13:33:54 -0500

    updated struct data types

--- pkg/email/data.go
+++ pkg/email/data.go
@@ -1,7 +1,5 @@
 package email
 
-import "time"
-
 type WelcomeData struct {
 	Company      string `json:"company" example:"Adomate"`
 	Domain       string `json:"domain" example:"adomate.ai"`
@@ -15,17 +13,17 @@ type PasswordResetData struct {
 }
 
 type InvoiceCreatedData struct {
-	Company            string    `json:"company" example:"Adomate"`
-	AmountDue          float64   `json:"amount_due" example:"18.34"`
-	DueAt              time.Time `json:"due_at" example:"2020-01-01T00:00:00Z"`
-	InvoiceLink        string    `json:"invoice_link" example:"https://app.adomate.ai/reset-password/1234"`
-	InvoiceDescription string    `json:"invoice_description" example:"August monthly service charge"`
+	Company            string `json:"company" example:"Adomate"`
+	AmountDue          string `json:"amount_due" example:"18.34"`
+	DueAt              string `json:"due_at" example:"2020-01-01T00:00:00Z"`
+	InvoiceLink        string `json:"invoice_link" example:"https://app.adomate.ai/reset-password/1234"`
+	InvoiceDescription string `json:"invoice_description" example:"August monthly service charge"`
 }
 
 type InvoicePaidData struct {
-	Company            string  `json:"company" example:"Adomate"`
-	AmountPaid         float64 `json:"amount_paid" example:"18.34"`
-	AmountRemaining    float64 `json:"amount_remaining" example:"18.34"`
-	InvoiceLink        string  `json:"invoice_link" example:"https://app.adomate.ai/reset-password/1234"`
-	InvoiceDescription string  `json:"invoice_description" example:"August monthly service charge"`
+	Company            string `json:"company" example:"Adomate"`
+	AmountPaid         string `json:"amount_paid" example:"18.34"`
+	AmountRemaining    string `json:"amount_remaining" example:"18.34"`
+	InvoiceLink        string `json:"invoice_link" example:"https://app.adomate.ai/reset-password/1234"`
+	InvoiceDescription string `json:"invoice_description" example:"August monthly service charge"`
 }
